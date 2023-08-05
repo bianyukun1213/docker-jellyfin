bdc04197716887b2fbc898c5d703a4349a669397
Hollis <bianyukun1213@outlook.com>
2023-08-05 15:59:02 +0800

    修改 test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 chmod +x $CURRENT_DIR/scripts/run.sh
 sudo docker build -t $1 .
-sudo docker run -d --network="bridge" -p 8096:8096 -v /config:$CURRENT_DIR/test_env $1
+sudo docker run -d --network="bridge" -p 8096:8096 -v $CURRENT_DIR/test_config:/config $1
