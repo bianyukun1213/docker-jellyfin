9677b0567096709dc8fbf493cfac57a5dd08457c
边宇琨 <bianyukun1213@outlook.com>
2024-01-07 14:11:32 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 chmod +x $CURRENT_DIR/scripts/run.sh
-sudo docker build -t $1 .
-sudo docker run -d --network="bridge" -p 8096:8096 -v $CURRENT_DIR/test_config:/config $1
+sudo docker build -t heyhollis/jellyfin:$1 .
+sudo docker run -d --network="bridge" -p 8096:8096 -v $CURRENT_DIR/test_config:/config heyhollis/jellyfin:$1
